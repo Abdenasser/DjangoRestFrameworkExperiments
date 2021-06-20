eb13c7cfd84c866dd7a4fc8cc2e77bf83fab5090
Abdenasser <nasser.elidrissi065@gmail.com>
2021-06-20 14:46:12 +0100

    comments for logic i don't care about that much

--- snippets/models.py
+++ snippets/models.py
@@ -5,6 +5,7 @@ from pygments.lexers import get_lexer_by_name
 from pygments.formatters.html import HtmlFormatter
 from pygments import highlight
 
+#just for code highlight
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
@@ -22,7 +23,7 @@ class Snippet(models.Model):
 
     class Meta:
         ordering = ['created']
-
+    #just for code highlight
     def save(self, *args, **kwargs):
         """
         Use the `pygments` library to create a highlighted HTML
